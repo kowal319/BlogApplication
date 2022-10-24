e9ff05c7572ac7284e8e2daac2509886f03afd83
kowal319 <bartlomiej319@wp.pl>
2022-10-24 16:34:31 +0200

    Post serive interface and post service implementation added with method to find all posts

---
+++ src/main/java/com/example/BlogApplication/service/Implementation/PostServiceImplementation.java
@@ -0,0 +1,29 @@
+package com.example.BlogApplication.service.Implementation;
+
+import com.example.BlogApplication.dto.PostDto;
+import com.example.BlogApplication.entity.Post;
+import com.example.BlogApplication.mapper.PostMapper;
+import com.example.BlogApplication.repository.PostRepository;
+import com.example.BlogApplication.service.PostService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+public class PostServiceImplementation implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImplementation(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public List<PostDto> findAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(PostMapper::mapToPostDto)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/example/BlogApplication/service/PostService.java
@@ -0,0 +1,9 @@
+package com.example.BlogApplication.service;
+
+import com.example.BlogApplication.dto.PostDto;
+
+import java.util.List;
+
+public interface PostService {
+    List<PostDto> findAllPosts();
+}
