8ac556224a29fc4417460e180e23411cd0bcfa20
kowal319 <bartlomiej319@wp.pl>
2022-10-26 15:56:54 +0200

    controller and service  to save posts done with getUrl

--- src/main/java/com/example/BlogApplication/controller/PostController.java
+++ src/main/java/com/example/BlogApplication/controller/PostController.java
@@ -6,6 +6,8 @@ import com.example.BlogApplication.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -33,4 +35,19 @@ public class PostController {
         model.addAttribute("post", postDto);
         return "admin/create_post";
  }
+
+ //method to handle form submit request
+    @PostMapping("/admin/posts")
+    public String createPost(@ModelAttribute PostDto postDto){
+        postDto.setUrl(getUrl(postDto.getTitle()));
+        postService.createPost(postDto);
+        return "redirect:/admin/posts";
+
+    }
+    private static String getUrl(String postTitle){
+        String title = postTitle.trim().toLowerCase();
+        String url = title.replaceAll("\\s+","-");
+        url = url.replaceAll("[A-Za-z0-9]", "-");
+        return url;
+    }
 }
