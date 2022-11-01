c36d3c5f48a83859c0e476431b31ae6ef867d3bf
kowal319 <bartlomiej319@wp.pl>
2022-11-01 17:13:20 +0100

    view post added

--- src/main/java/com/example/BlogApplication/controller/BlogController.java
+++ src/main/java/com/example/BlogApplication/controller/BlogController.java
@@ -5,6 +5,7 @@ import com.example.BlogApplication.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,5 +23,14 @@ public class BlogController{
         List<PostDto> postsResponse = postService.findAllPosts();
         model.addAttribute("postsResponse", postsResponse);
         return "blog/view_posts";
+    }
+
+    //handler method to handle view post request
+    @GetMapping("/post/{postUrl}")
+    public String showPost(@PathVariable("postUrl") String postUrl,
+                           Model model){
+        PostDto post = postService.findPostByUrl(postUrl);
+        model.addAttribute("post", post);
+        return "blog/blog_post";
     }
 }
