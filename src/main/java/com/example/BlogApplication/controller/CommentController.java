f4e4e820d41cf75d13a42db8ae1b7cfcda12acba
kowal319 <bartlomiej319@wp.pl>
2022-11-03 12:20:28 +0100

    validation added

--- src/main/java/com/example/BlogApplication/controller/CommentController.java
+++ src/main/java/com/example/BlogApplication/controller/CommentController.java
@@ -1,9 +1,13 @@
 package com.example.BlogApplication.controller;
 
 import com.example.BlogApplication.dto.CommentDto;
+import com.example.BlogApplication.dto.PostDto;
 import com.example.BlogApplication.service.CommentService;
+import com.example.BlogApplication.service.PostService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,15 +15,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class CommentController {
     private CommentService commentService;
+     private PostService postService;
 
-    public CommentController(CommentService commentService) {
+    public CommentController(CommentService commentService, PostService postService) {
         this.commentService = commentService;
+        this.postService = postService;
     }
 
     @PostMapping("/{postUrl}/comments")
     public String createComment(@PathVariable("postUrl") String postUrl,
+                                @Valid
                                 @ModelAttribute("comment") CommentDto commentDto,
+                                BindingResult result,
                                 Model model){
+        PostDto postDto = postService.findPostByUrl(postUrl);
+        if(result.hasErrors()){
+            model.addAttribute("post", postDto);
+            model.addAttribute("comment", commentDto);
+            return "blog/blog_post";
+        }
         commentService.createComment(postUrl, commentDto);
         return "redirect:/post/" + postUrl;
     }
--- src/main/java/com/example/BlogApplication/dto/CommentDto.java
+++ src/main/java/com/example/BlogApplication/dto/CommentDto.java
@@ -1,5 +1,7 @@
 package com.example.BlogApplication.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,8 +13,12 @@ import java.time.LocalDateTime;
 @Builder
 public class CommentDto {
     private Long id;
+    @NotEmpty(message = "Name must not be empty")
     private String name;
+    @NotEmpty(message = "Email must not be empty")
+    @Email
     private String email;
+    @NotEmpty(message = "Content must not be empty")
     private String content;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
