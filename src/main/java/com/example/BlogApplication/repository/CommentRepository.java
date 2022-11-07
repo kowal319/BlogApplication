09ffc9ae6fd7b1c709976e39f5dc5c04d8a6ee72
kowal319 <bartlomiej319@wp.pl>
2022-11-07 15:29:26 +0100

    only logged user can see comments lists belongs to his post added

--- src/main/java/com/example/BlogApplication/repository/CommentRepository.java
+++ src/main/java/com/example/BlogApplication/repository/CommentRepository.java
@@ -2,6 +2,13 @@ package com.example.BlogApplication.repository;
 
 import com.example.BlogApplication.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository <Comment, Long> {
+
+
+    @Query(value = "select c.* from comments c inner join posts p where c.post_id = p.id and p.created_by =:userId ", nativeQuery = true)
+    List<Comment> findCommentsByPost(Long userId);
 }
--- src/main/java/com/example/BlogApplication/service/Implementation/CommentServiceImplementation.java
+++ src/main/java/com/example/BlogApplication/service/Implementation/CommentServiceImplementation.java
@@ -3,10 +3,13 @@ package com.example.BlogApplication.service.Implementation;
 import com.example.BlogApplication.dto.CommentDto;
 import com.example.BlogApplication.entity.Comment;
 import com.example.BlogApplication.entity.Post;
+import com.example.BlogApplication.entity.User;
 import com.example.BlogApplication.mapper.CommentMapper;
 import com.example.BlogApplication.repository.CommentRepository;
 import com.example.BlogApplication.repository.PostRepository;
+import com.example.BlogApplication.repository.UserRepository;
 import com.example.BlogApplication.service.CommentService;
+import com.example.BlogApplication.util.SecurityUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,10 +20,12 @@ public class CommentServiceImplementation implements CommentService {
 
     private CommentRepository commentRepository;
     private PostRepository postRepository;
+    private UserRepository userRepository;
 
-    public CommentServiceImplementation(CommentRepository commentRepository, PostRepository postRepository) {
+    public CommentServiceImplementation(CommentRepository commentRepository, PostRepository postRepository, UserRepository userRepository) {
         this.commentRepository = commentRepository;
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -45,5 +50,15 @@ public class CommentServiceImplementation implements CommentService {
     @Override
     public void deleteComment(Long commentId) {
         commentRepository.deleteById(commentId);
+    }
+
+    @Override
+    public List<CommentDto> findCommentsByPost() {
+        String email = SecurityUtils.getCurrentUser().getUsername();
+        User createdBy = userRepository.findByEmail(email);
+        Long userId = createdBy.getId();
+        List<Comment> comments = commentRepository.findCommentsByPost(userId);
+        return comments.stream().map((comment) -> CommentMapper.mapToCommentDto(comment))
+                .collect(Collectors.toList());
     }
 }
